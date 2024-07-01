ba99aae4b9f7202813d59fca2525036986c83fca
NomansCraft <mr.noman1971@gmail.com>
2024-07-01 10:38:02 +0600

    Pre Test Loop

---
+++ src/noman/love/java/chapter4/examples/PreTestLoop.java
@@ -0,0 +1,15 @@
+package noman.love.java.chapter4.examples;
+
+public class PreTestLoop {
+
+    public static void main(String[] args) {
+        System.out.println("Testing while loop");
+        int i = 5;
+
+        while (i > 10) {
+            System.out.println("This will not print");
+        }
+
+        System.out.println("While loop didn't work");
+    }
+}
