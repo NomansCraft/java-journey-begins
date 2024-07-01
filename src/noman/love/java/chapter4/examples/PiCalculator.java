6f06882b61f31f0649df003bcef869c69e6acdbe
NomansCraft <mr.noman1971@gmail.com>
2024-07-01 10:31:10 +0600

    Calculates the value of π (Pi) using a series approximation method involving numerator and denominator

---
+++ src/noman/love/java/chapter4/examples/PiCalculator.java
@@ -0,0 +1,18 @@
+package noman.love.java.chapter4.examples;
+
+public class PiCalculator {
+
+    public double calculate (int nTerms) {
+        double numerator = 4.0;
+        double denominator = 1.0;
+        double sign = 1.0;
+
+        double pi = 0.0;
+        for (int i = 0; i < nTerms; i++) {
+            pi += sign * (numerator / denominator);
+            denominator += 2.0;
+            sign *= -1.0;
+        }
+        return pi;
+    }
+}
---
+++ src/noman/love/java/chapter4/examples/PiCalculatorDemo.java
@@ -0,0 +1,10 @@
+package noman.love.java.chapter4.examples;
+
+public class PiCalculatorDemo {
+
+    public static void main(String[] args) {
+        PiCalculator piCalculator = new PiCalculator();
+        var pi = piCalculator.calculate(100_000);
+        System.out.println("Pi = " + pi);
+    }
+}
