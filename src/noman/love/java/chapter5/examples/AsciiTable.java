7655ed9fbd3a9f41ee57b5a00094476c460fd9da
NomansCraft <mr.noman1971@gmail.com>
2024-07-07 17:30:01 +0600

    ASCII Table

---
+++ src/noman/love/java/chapter5/examples/AsciiTable.java
@@ -0,0 +1,10 @@
+package noman.love.java.chapter5.examples;
+
+public class AsciiTable {
+
+    public static void main(String[] args) {
+        char a = 'a';
+        int asciiValueOfA = (int) a;
+        System.out.println(asciiValueOfA);
+    }
+}
