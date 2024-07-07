754f6327fa08838a07eff72cc2ba76d1b4947121
NomansCraft <mr.noman1971@gmail.com>
2024-07-07 13:48:01 +0600

    'length' field of an array

---
+++ src/noman/love/java/chapter5/examples/ArrayExample.java
@@ -0,0 +1,9 @@
+package noman.love.java.chapter5.examples;
+
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        int[] myArray = {-45, 1, 8, 4, 5, 4, 5, 105};
+        System.out.println("Total items in the array is: " + myArray.length);
+    }
+}
