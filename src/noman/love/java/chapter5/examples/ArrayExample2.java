c7d3fe305c7237703286e17c6aeede1a39b25f91
NomansCraft <mr.noman1971@gmail.com>
2024-07-07 13:55:10 +0600

    Array processing with 'for' loop

---
+++ src/noman/love/java/chapter5/examples/ArrayExample2.java
@@ -0,0 +1,14 @@
+package noman.love.java.chapter5.examples;
+
+public class ArrayExample2 {
+
+    public static void main(String[] args) {
+        int[] myArray = {-45, 1, 8, 4, 5, 4, 5, 105};
+
+        System.out.println("Index\tValue");
+
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(i + "\t\t" + myArray[i]);
+        }
+    }
+}
