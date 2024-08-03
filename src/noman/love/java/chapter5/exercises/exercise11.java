69aa0d7c75caee07a81db44d41dc192c500c777b
NomansCraft <mr.noman1971@gmail.com>
2024-08-03 17:07:18 +0600

    "Added a program to collect and quiz country information
     - Implemented data collection for country information including name, capital, currency, and language.
     - Converted all input to lowercase to ensure case-insensitive comparison.
     - Added a questionnaire to quiz the user about the collected country information.
     - Provided immediate feedback on the user's answers using checkmarks (✔) and crosses (✖).
     - Printed a summary of the total correct and wrong answers at the end."

--- src/noman/love/java/chapter5/exercises/exercise11.java
+++ src/noman/love/java/chapter5/exercises/exercise11.java
@@ -12,24 +12,69 @@ public class exercise11 {
         for (int i = 0; i < countryInfo.length; i++) {
             System.out.println("Enter information for country " + (i + 1) + ":");
 
-            System.out.print("Enter country name: ");
-            countryInfo[i][0] = scanner.next();
+            System.out.println("Enter country name: ");
+            countryInfo[i][0] = scanner.next().toLowerCase();
 
-            System.out.printf("Enter capital of %s: ", countryInfo[i][0]);
-            countryInfo[i][1] = scanner.next();
+            System.out.println("Enter capital of " + countryInfo[i][0] + ":");
+            countryInfo[i][1] = scanner.next().toLowerCase();
 
-            System.out.printf("Enter currency of %s: ", countryInfo[i][0]);
-            countryInfo[i][2] = scanner.next();
+            System.out.println("Enter currency of " + countryInfo[i][0] + ":");
+            countryInfo[i][2] = scanner.next().toLowerCase();
 
-            System.out.printf("Enter language of %s: ", countryInfo[i][0]);
-            countryInfo[i][3] = scanner.next();
+            System.out.println("Enter language of " + countryInfo[i][0] + ":");
+            countryInfo[i][3] = scanner.next().toLowerCase();
         }
 
         System.out.println("Collected country information:");
         for (int i = 0; i < countryInfo.length; i++) {
-            System.out.printf("Country %d: name: %s, Capital: %s, Currency: %s, Language: %s%n",
+            System.out.printf("Country %d: Name: %s, Capital: %s, Currency: %s, Language: %s%n",
                     i + 1, countryInfo[i][0], countryInfo[i][1], countryInfo[i][2], countryInfo[i][3]);
         }
+
+        System.out.println();
+        // Question section
+        int correctAnswer = 0;
+        int wrongAnswer = 0;
+
+        for (int i = 0; i < countryInfo.length; i++) {
+            // First Question
+            System.out.println("Q: What is the capital of " + countryInfo[i][0] + "?");
+            String answer = scanner.next().toLowerCase();
+            if (answer.equals(countryInfo[i][1])) {
+                System.out.println("✔ Well Done!");
+                correctAnswer++;
+            } else {
+                System.out.println("✖ The correct answer should be " + countryInfo[i][1]);
+                wrongAnswer++;
+            }
+
+            // Second Question
+            System.out.println("Q: What is the currency of "+ countryInfo[i][0] + "?");
+            answer = scanner.next().toLowerCase();
+            if (answer.equals(countryInfo[i][2])) {
+                System.out.println("✔ Well Done!");
+                correctAnswer++;
+            } else {
+                System.out.println("✖ The correct answer should be " + countryInfo[i][2]);
+                wrongAnswer++;
+            }
+
+            // Third Question
+            System.out.println("Q: What is the language of " + countryInfo[i][0] + "?");
+            answer = scanner.next().toLowerCase();
+            if (answer.equals(countryInfo[i][3])) {
+                System.out.println("✔ Well Done!");
+                correctAnswer++;
+            } else {
+                System.out.println("✖ The correct answer should be " + countryInfo[i][3]);
+                wrongAnswer++;
+            }
+
+        }
+
+        System.out.println("-------------------------");
+        System.out.println("Total correct answer: " + correctAnswer);
+        System.out.println("Total wrong answer: " + wrongAnswer);
         scanner.close();
     }
 }
