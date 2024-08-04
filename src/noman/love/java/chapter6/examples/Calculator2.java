46304be7bece86ddf165a0e812fb43e469c4a0d6
NomansCraft <mr.noman1971@gmail.com>
2024-08-04 22:11:09 +0600

    Method overloading

---
+++ src/noman/love/java/chapter6/examples/Calculator2.java
@@ -0,0 +1,19 @@
+package noman.love.java.chapter6.examples;
+
+public class Calculator2 {
+
+    public int performAddition(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+    public float performAddition(float a, float b) {
+        float result = a + b;
+        return result;
+    }
+
+    public double performAddition(double a, double b) {
+        double result = a + b;
+        return result;
+    }
+}
