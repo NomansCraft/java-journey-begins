02a9c16990dd5b677fbcfe09e256a290762bb053
NomansCraft <mr.noman1971@gmail.com>
2024-08-04 22:32:51 +0600

    "Add Person class and PersonDemo to demonstrate constructor overloading"

--- src/noman/love/java/chapter6/examples/Person.java
+++ src/noman/love/java/chapter6/examples/Person.java
@@ -5,6 +5,15 @@ public class Person {
     private String email;
     private int age;
 
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
     public Person(String name, String email, int age) {
         this.name = name;
         this.email = email;
--- src/noman/love/java/chapter6/examples/PersonDemo.java
+++ src/noman/love/java/chapter6/examples/PersonDemo.java
@@ -4,6 +4,11 @@ public class PersonDemo {
 
     public static void main(String[] args) {
         Person noman = new Person("Noman", "mr.noman1971@gmail.com", 38);
+        Person monir = new Person("Lenin");
+        Person ohiduzzaman = new Person("Md. Ohiduzzaman", "example@gmail.com");
+
         noman.printInfo();
+        monir.printInfo();
+        ohiduzzaman.printInfo();
     }
 }
