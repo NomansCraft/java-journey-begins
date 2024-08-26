83af77cbd63f2a21152332ba7323880df3969569
NomansCraft <mr.noman1971@gmail.com>
2024-08-26 16:48:08 +0600

    "Static field example"

---
+++ src/noman/love/java/chapter6/examples/Calculator3.java
@@ -0,0 +1,8 @@
+package noman.love.java.chapter6.examples;
+
+public class Calculator3 {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/noman/love/java/chapter6/examples/CalculatorDemo3.java
@@ -0,0 +1,10 @@
+package noman.love.java.chapter6.examples;
+
+public class CalculatorDemo3 {
+
+    public static void main(String[] args) {
+        int result = Calculator3.add(3, 5);
+
+        System.out.println("3 + 5 = " + result);
+    }
+}
