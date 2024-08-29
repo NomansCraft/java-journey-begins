0f7913b9beeec74d3c0e31457a8823c632a06c48
NomansCraft <mr.noman1971@gmail.com>
2024-08-29 22:50:13 +0600

    "Method 'Override'"

--- src/noman/love/java/chapter7/examples/SavingAccount.java
+++ src/noman/love/java/chapter7/examples/SavingAccount.java
@@ -1,10 +1,27 @@
 package noman.love.java.chapter7.examples;
 
 public class SavingAccount extends CheckingAccount {
+    private static final int MAX_WITHDRAWAL = 5;
+
     private final double interestRate;
+    private int withdrawalCount;
 
     public SavingAccount(int initialBalance, String accountNumber, double interestRate) {
         super(initialBalance, accountNumber);
         this.interestRate = interestRate;
+    }
+
+    public double calculateInterest() {
+        return getBalance() * interestRate / 100;
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        if (withdrawalCount == MAX_WITHDRAWAL) {
+            System.out.println("Withdrawal limit exceed.");
+        } else {
+            super.withdraw(amount);
+            withdrawalCount++;
+        }
     }
 }
