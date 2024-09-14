3d9f77eeec4e645269656afd81b18974eb72fcc2
NomansCraft <mr.noman1971@gmail.com>
2024-09-14 14:26:44 +0600

    "Using pattern matching instead of explicit casting"

--- src/noman/love/java/chapter7/examples/upcastinganddowncasting/DogWalker.java
+++ src/noman/love/java/chapter7/examples/upcastinganddowncasting/DogWalker.java
@@ -2,8 +2,7 @@ package noman.love.java.chapter7.examples.upcastinganddowncasting;
 
 public class DogWalker {
     public void walk(Dog dog) {
-        if (dog instanceof Poodle) {
-            Poodle poodle = (Poodle) dog;
+        if (dog instanceof Poodle poodle) {
             System.out.println("Walking " + poodle.getName());
         } else {
             System.out.println("Walking " + dog.getBreed());
